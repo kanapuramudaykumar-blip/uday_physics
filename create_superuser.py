78aed9a93a30de8d2db1ec40b57b312c78482dff
kanapuramudaykumar-blip <kanapuram.udaykumar@gmail.com>
2025-09-22 15:31:24 +0530

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -1,14 +1,14 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogApp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "professor_website.settings")
 django.setup()
 
 from django.contrib.auth.models import User
 
-username = "your_username"
+username = "Udat4Bharath"
 email = "example@gmail.com"
-password = "your_password"
+password = "Chinmayi@2020"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
