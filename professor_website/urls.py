11fc31e0b4ce580c7e85660a4ea8a88649472e45
kanapuramudaykumar-blip <kanapuram.udaykumar@gmail.com>
2025-11-26 11:56:26 +0530

    Update urls.py

--- professor_website/urls.py
+++ professor_website/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from website.views import backup_db_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('backup-db/', backup_db_view, name='backup-db'),
     path('', include('website.urls')),
 ]
 
